dc2af4503fef86152353246d726b0cc5cbadd7ef
Enzo Taurines <enzodylan.taurines@gmail.com>
2024-09-20 17:05:15 +0200

    Implement authContext with token validation and persistence
    - Automatically insert Bearer token in API requests when authenticated
    - Verify token validity on each request, auto-logout on token expiration
    - Use authContext for data persistence across the app, including page refresh via LocalStorage
    - Login and registration functionalities work correctly, providing instant access to the site

--- src/App.jsx
+++ src/App.jsx
@@ -10,29 +10,35 @@ import Event from "./views/event";
 import "./styles/variables.scss";
 import ProfilClient from "./views/profilClient";
 import Auth from "./views/auth";
+import Logout from "./components/logout";
+
+import { authProvider as AuthProvider } from "./context/authProvider";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Helmet>
-          <title>Event</title>
-          <meta name="description" content="Event" />
-          <link href="./output.css" rel="stylesheet"></link>
-        </Helmet>
-        <Header />
-        <Routes>
-          <Route index path={"/"} element={<Home />} />
-          <Route path={"/cities"} element={<Cities />} />
-          <Route path={"/cities/:city"} element={<Cities />} />x
-          <Route path={"/cities/:city"} element={<City />} />x
-          <Route path={"/cities/:city/:eventId"} element={<Event />} />
-          {/* a modifier quand le back est fini */}
-          <Route path={"/profileclient"} element={<ProfilClient />} />
-          <Route path={"/login"} element={<Auth />} />
-        </Routes>
-        {/* <Footer /> */}
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Helmet>
+            <title>Event</title>
+            <meta name="description" content="Event" />
+            <link href="./output.css" rel="stylesheet"></link>
+          </Helmet>
+          <Header />
+          <Routes>
+            <Route index path={"/"} element={<Home />} />
+            <Route path={"/cities"} element={<Cities />} />
+            <Route path={"/cities/:city"} element={<Cities />} />x
+            <Route path={"/cities/:city"} element={<City />} />x
+            <Route path={"/cities/:city/:eventId"} element={<Event />} />
+            {/* a modifier quand le back est fini */}
+            <Route path={"/profileclient"} element={<ProfilClient />} />
+            <Route path={"/login"} element={<Auth />} />
+            <Route path="/logout" element={<Logout />} />
+          </Routes>
+          {/* <Footer /> */}
+        </BrowserRouter>
+      </AuthProvider>
     </>
   );
 }
--- src/views/profilClient.jsx
+++ src/views/profilClient.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from "react";
 import "../styles/profilClient.scss";
-
+import { Link } from "react-router-dom";
 
 export default function ProfilClient() {
   return (
-    <div>profilClient</div>
-  )
+    <section className="page-container">
+      <div>profilClient</div>
+      <Link className="" to={"/logout"}>
+        se déconnecter
+      </Link>
+    </section>
+  );
 }
-
