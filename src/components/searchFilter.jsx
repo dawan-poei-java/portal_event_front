9709aa188980116be03b59dc53cd8425e7fcf166
Dorian Gautier <gautier.dor@gmail.com>
2024-09-26 15:46:39 +0200

    Co-authored-by: seezix <seezix@users.noreply.github.com>

--- src/components/searchFilter.jsx
+++ src/components/searchFilter.jsx
@@ -1,32 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 
-// Exemple de données d'événements
-
-const EventSearch = ({ eventsData, onFilter }) => {
+const SearchFilter = ({ eventsData, onFilter, selectedCategory }) => {
   const [searchTerm, setSearchTerm] = useState("");
-  const [filteredEvents, setFilteredEvents] = useState(eventsData);
 
-  // Fonction de gestion de la recherche
   const handleSearch = (e) => {
     const value = e.target.value.toLowerCase();
     setSearchTerm(value);
-
-    // Filtrage des événements en fonction du mot-clé saisi
-
-    //setFilteredEvents(filtered);
   };
 
   useEffect(() => {
-    const filtered = eventsData.filter(
-      (event) =>
-        event.title.toLowerCase().includes(searchTerm) ||
-        event.description.toLowerCase().includes(searchTerm)
-    );
+    let filtered = eventsData;
+
+    if (selectedCategory !== "Tout") {
+      filtered = filtered.filter(
+        (event) => event.category === selectedCategory
+      );
+    }
+
+    if (searchTerm) {
+      filtered = filtered.filter(
+        (event) =>
+          event.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          event.description.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    }
+
     onFilter(filtered);
-  }, [eventsData, searchTerm]);
+  }, [searchTerm, selectedCategory]);
+
   return (
     <div>
-      <h1>Recherche d'événements</h1>
       <input
         type="text"
         placeholder="Rechercher un événement..."
@@ -34,21 +37,8 @@ const EventSearch = ({ eventsData, onFilter }) => {
         onChange={handleSearch}
         style={{ padding: "10px", width: "100%", marginBottom: "20px" }}
       />
-
-      {filteredEvents.length > 0 ? (
-        <ul>
-          {filteredEvents.map((event) => (
-            <li key={event.id}>
-              <h2>{event.title}</h2>
-              <p>{event.description}</p>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>Aucun événement trouvé.</p>
-      )}
     </div>
   );
 };
 
-export default EventSearch;
+export default SearchFilter;
--- src/views/listAllEvent.jsx
+++ src/views/listAllEvent.jsx
@@ -1,61 +1,85 @@
 import "../styles/home.scss";
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "../styles/listAllEvent.scss";
-import EventCard from "../components/eventCard"
+import EventCard from "../components/eventCard";
 import EventGrid from "../components/eventGrid";
 import SearchFilter from "../components/searchFilter";
 
 export default function ListAllEvent() {
   const allEvents = [
-    { id: 1, title: "Atelier de peinture", category: "Spectacle" },
-    { id: 2, title: "Concert de Jazz", category: "Concert" },
-    { id: 3, title: "Exposition d'Art Moderne", category: "Exposition" },
+    {
+      id: 1,
+      title: "Atelier de peinture",
+      category: "Spectacle",
+      description: "description",
+    },
+    {
+      id: 2,
+      title: "Concert de Jazz",
+      category: "Concert",
+      description: "description",
+    },
+    {
+      id: 3,
+      title: "Exposition d'Art Moderne",
+      category: "Exposition",
+      description: "description",
+    },
+    {
+      id: 4,
+      title: "Concert de Rock",
+      category: "Concert",
+      description: "description",
+    },
   ];
 
   const [selectedCategory, setSelectedCategory] = useState("Tout");
-
   const [filteredEvents, setFilteredEvents] = useState(allEvents);
 
+  const handleFilterEvent = (events) => {
+    setFilteredEvents(events);
+  };
 
-  const handleFilterEvent = useCallback((EventFilter) => {
-    setFilteredEvents(EventFilter);
-  }, []);
   return (
     <>
       <section className="page-container-allEvent">
         <h2>Tout les évènements</h2>
         <div className="flex justify-between ">
-          <SearchFilter eventsData={allEvents} onFilter={handleFilterEvent} />
+          <SearchFilter
+            eventsData={allEvents}
+            onFilter={handleFilterEvent}
+            selectedCategory={selectedCategory}
+          />
           <div className="flex gap-2">
             <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+              className="px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700"
               onClick={() => setSelectedCategory("Spectacle")}
             >
               Spectacle
             </button>
             <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+              className="px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700"
               onClick={() => setSelectedCategory("Concert")}
             >
               Concert
             </button>
             <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+              className="px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700"
               onClick={() => setSelectedCategory("Exposition")}
             >
               Exposition
             </button>
             <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+              className="px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700"
               onClick={() => setSelectedCategory("Tout")}
             >
               Tout
             </button>
           </div>
         </div>
-        <div className="flex flex-warp warp event-container-soon justify-between">
-          {allEvents.map((e) => {
-            return <EventCard />;
+        <div className="flex justify-between flex-warp warp event-container-soon">
+          {filteredEvents.map((e) => {
+            return <EventCard key={e.id} />;
           })}
         </div>
 
